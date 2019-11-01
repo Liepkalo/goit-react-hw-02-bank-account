23f2958bf579f28804b5e69f141cc486a653bbba
Oleg Liepkalo <liepkalo.o@gmail.com>
2019-11-01 23:31:28 +0200

    Updates

---
+++ precache-manifest.20e08f507ac7ec53b07d4179823b76fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c3beff12f199f1656913bcc0814d98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc1487d56155cc80f09d",
+    "url": "/static/css/main.38f01b55.chunk.css"
+  },
+  {
+    "revision": "ba35ea0748574d89d273",
+    "url": "/static/js/2.562396ca.chunk.js"
+  },
+  {
+    "revision": "dc1487d56155cc80f09d",
+    "url": "/static/js/main.6c6f9f13.chunk.js"
+  },
+  {
+    "revision": "95191575c9c1a0df9b74",
+    "url": "/static/js/runtime-main.d4e071dc.js"
+  }
+]);
